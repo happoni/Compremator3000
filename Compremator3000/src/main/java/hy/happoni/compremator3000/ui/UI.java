4e55247a392bde3c9cef4d33b71c94c623e4ec59
Niko Häppölä <niko.happola@gmail.com>
2019-09-18 19:51:06 +0300

    Käliä aloiteltu.

--- Compremator3000/src/main/java/hy/happoni/compremator3000/ui/UI.java
+++ Compremator3000/src/main/java/hy/happoni/compremator3000/ui/UI.java
@@ -37,32 +37,58 @@ public class UI {
             System.out.println("[1] -- Use test text \"TOBEORNOTTOBEORTOBEORNOT\"");
             System.out.println("[2] -- Insert your own text to be compressed");
             System.out.println("[x] -- Exit program");
-            
+
             System.out.print("> ");
             String choice = reader.nextLine();
-            
+
             if (choice.equals("1")) {
                 useTestText();
             } else if (choice.equals("2")) {
                 insertOwnText();
             } else if (choice.equals("x")) {
                 break;
-            }                        
+            }
         }
     }
-    
+
     /**
      * Metodi...
      */
     private void useTestText() {
-        
+        while (true) {
+            System.out.println("Text is \"TOBEORNOTTOBEORTOBEORNOT\"");
+            System.out.println("What algorithm shall we use to compress it?");
+            System.out.println("[1] -- LZ77");
+            System.out.println("[2] -- LZW");
+            System.out.println("[3] -- LZSS");
+            System.out.println("[4] -- Compress with all.");
+            System.out.println("[x] -- Back to main menu.");
+
+            System.out.println("> ");
+            String choice = reader.nextLine();
+            String testText = "TOBEORNOTTOBEORTOBEORNOT";
+            
+            if (choice.equals("1")) {
+                useLZ(testText);
+            } else if (choice.equals("2")) {
+                useLZW(testText);
+            } else if (choice.equals("3")) {
+                useLZSS(testText);
+            } else if (choice.equals("4")) {
+                useAll(testText);
+            } else if (choice.equals("x")) {
+                mainMenu();
+            }
+        }
     }
+
+
     
     /**
      * Metodi...
      */
     private void insertOwnText() {
-        
+
     }
-    
+
 }

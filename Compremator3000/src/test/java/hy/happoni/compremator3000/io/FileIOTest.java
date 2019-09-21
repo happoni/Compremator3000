a185e38c18ee7551a76b66caba028a24b6b22e30
Niko Häppölä <niko.happola@gmail.com>
2019-09-21 14:06:30 +0300

    FileIO:lle luotu testejä.

--- Compremator3000/src/test/java/hy/happoni/compremator3000/io/FileIOTest.java
+++ Compremator3000/src/test/java/hy/happoni/compremator3000/io/FileIOTest.java
@@ -1,22 +1,50 @@
 // pakkaus
 package hy.happoni.compremator3000.io;
 
+import java.io.File;
+import java.io.FileWriter;
+import org.junit.After;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * Testiluokka tiedostonlukijalle.
  */
 public class FileIOTest {
-    
-    public FileIOTest() {
-    }
-    
 
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+    
+    File testFile;
+    FileIO fileIO;
+    String testPath;
+    
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        fileIO = new FileIO();
+        testFile = testFolder.newFile("testfile.txt");
+        
+        try (FileWriter file = new FileWriter(testFile.getAbsolutePath())) {
+            file.write("Mississippi on iso joki jossa isi soutaa.");
+        }   
+        testPath = testFile.getAbsolutePath();
     }
 
-
-    // @Test
-    // public void hello() {}
+    @Test
+    public void readFileReturnsCorrectText() {
+        String correct = "Mississippi on iso joki jossa isi soutaa.";
+    
+        String test = fileIO.readFile(testPath);        
+        assertTrue(correct.equals(test));
+        assertFalse(test.contains("666"));
+    }
+    
+    @After
+    public void tearDown() {
+        testFile.delete();
+    }
 }

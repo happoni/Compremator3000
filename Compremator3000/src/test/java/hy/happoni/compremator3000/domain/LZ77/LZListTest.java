8dc85ab5de1bf790f1c2fb073ae97dc9ea4845b5
Niko Häppölä <niko.happola@gmail.com>
2019-10-15 11:08:31 +0300

    Listalle testejä.

--- Compremator3000/src/test/java/hy/happoni/compremator3000/domain/LZ77/LZListTest.java
+++ Compremator3000/src/test/java/hy/happoni/compremator3000/domain/LZ77/LZListTest.java
@@ -12,11 +12,52 @@ import static org.junit.Assert.*;
  */
 public class LZListTest {
 
+    LZList list;
+    
     public LZListTest() {
     }
 
     @Before
     public void setUp() {
+        list = new LZList();
+        list.add(new Tuple(1, 2, "o"));
     }
 
+    /**
+     * Testillä tarkistetaan, että size-metodi antaa oikean arvon.
+     */
+    @Test
+    public void sizeReturnsCorrectValue() {
+        assertEquals(1, list.size());
+    }
+    
+    /**
+     * Testillä tarkistetaan, että listan sisäisen taulukon kokoa kasvatetaan, kun listalle lisätään paljon tupleja.
+     */
+    @Test
+    public void addingManyValuesIncreasesArraySize() {
+        for (int i = 0; i < 200; i++) {
+            list.add(new Tuple(1, i, "j"));
+        }
+        assertEquals(201, list.size());
+    }
+    
+    /**
+     * Testillä tarkistetaan, että listan get-metodi palauttaa oikean tuplen.
+     */
+    @Test
+    public void getReturnsCorrectTuple() {
+        assertEquals(1, list.get(0).offset);
+        assertEquals(2, list.get(0).stringLength);       
+    }
+    
+    /**
+     * Testillä tarkistetaan, että toByteArray-metodi toimii oikein.
+     */
+    @Test
+    public void toByteArrayReturnsCorrectValue() {
+        byte[] ba = list.toByteArray();
+        assertEquals(9, ba.length);
+    }
+    
 }
